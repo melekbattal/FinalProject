1e371ccaeaaf9dcd1909121e8c2e78430492c7b9
melek <melektulugbattal@gmail.com>
2021-02-25 22:01:46 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,63 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+
+            return new Result(true,"ürün eklendi");
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları 
+            //yetkisi var mı?
+
+            return _productDal.GetAll();           
+           
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice <= min &&  p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<ProductDetailDto> IProductService.GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
